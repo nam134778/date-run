e73e4209deaf8162357c75b39639c8d30b6a7323
namnt <62176476+nam134778@users.noreply.github.com>
2023-04-19 14:42:14 +0700

    Update

--- App.tsx
+++ App.tsx
@@ -4,11 +4,40 @@ import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
 import OnboardingScreen from './src/features/onboarding/onboardScreen';
 import DashboardScreen from './src/features/dashboard/dashboardScreen';
-
+import React from 'react';
+import { useState, useEffect } from 'react';
+import AsyncStorage from '@react-native-async-storage/async-storage';
 const AppStack = createStackNavigator();
 
 export default function App() {
+  const [firstLaunch, setFirstLaunch] = useState<boolean | undefined>();
+  useEffect(() => {
+    async function setData() {
+      const appData = await AsyncStorage.getItem("appLaunched");
+      if (appData == null) {
+        setFirstLaunch(true);
+        AsyncStorage.setItem("appLaunched", "false");
+      } else {
+        setFirstLaunch(false);
+      }
+    }
+    setData();
+  }, []);
   return (
+    // firstLaunch != null && (
+    //   <NavigationContainer>
+    //     <AppStack.Navigator screenOptions={{headerShown: false}}>
+    //       {firstLaunch && (
+    //         <AppStack.Screen
+    //           options={{ headerShown: false }}
+    //           name="Onboarding"
+    //           component={OnboardingScreen}
+    //         />
+    //       )}
+    //       <AppStack.Screen options={{ headerShown: false }} name="Dashboard" component={DashboardScreen} />
+    //     </AppStack.Navigator>
+    //   </NavigationContainer>
+    // )
     <NavigationContainer>
       <AppStack.Navigator screenOptions={{headerShown: false}}>
         <AppStack.Screen name="Onboarding" component={OnboardingScreen} />
--- src/features/dashboard/dashboardScreen.js
+++ src/features/dashboard/dashboardScreen.js
@@ -1,19 +1,73 @@
 import React from 'react';
-import { View, Text, Button, StyleSheet } from 'react-native';
-
+import { View, Text, Button, StyleSheet, SafeAreaView, ProgressBarAndroid, StatusBar, ScrollView } from 'react-native';
+// import { Stack, Avatar } from '@react-native-material/core';
+import Icon from 'react-native-vector-icons/Feather';
+const COLORS = {primary: "#FD539A", secondary: "#FFCCD3"};
+import { Avatar, LinearProgress } from 'react-native-elements';
+import { SafeAreaProvider } from 'react-native-safe-area-context';
 const DashboardScreen = ({navigation}) => {
     return (
-        <View style={styles.container}>
-            <Text>Dashboard Screen</Text>
-            <Button
-                title="Click Here"
-                onPress={() => alert("Button Clicked!")}
-            />
-        </View>
+        // <View style={styles.container}>
+        //     <Text>Dashboard Screen</Text>
+        //     <Button
+        //         title="Click Here"
+        //         onPress={() => alert("Button Clicked!")}
+        //     />
+        // </View>
+        // <SafeAreaProvider>
+        <SafeAreaView style={{ flex: 1, backgroundColor: '#F3F6FB'}}>
+            <StatusBar translucent={true} backgroundColor={COLORS.primary} />
+            <View style={styles.header}>
+                {/* <Icon name="settings" size={28} color={"white"} /> */}
+                <View>
+                    <Text style={{ marginTop: 60, marginLeft: 55, fontSize: 18, fontWeight: '700', color: 'white'}}>{'Mike Johnson'}</Text>
+                    <Avatar
+                        size={46}
+                        rounded
+                        icon={{name: 'user', type: 'font-awesome'}}
+                        onPress={() => console.log("Works!")}
+                        activeOpacity={0.1}
+                        containerStyle={{marginTop: -25, backgroundColor: "gray"}}
+                    />
+                </View>
+
+                <Icon name="settings" size={28} color={"white"} style={{marginTop: 60}}/>
+            </View>
+            <ScrollView showsVerticalScrollIndicator={false}>
+                <View style={{
+                    backgroundColor: COLORS.primary,
+                    height: 145,
+                    borderBottomLeftRadius: 18,
+                    borderBottomRightRadius: 18,
+                    paddingHorizontal: 30
+                }}>
+                    <View>
+                        <View style={styles.profileContainer} elevation={2}>
+                            <View style={{flex: 1, flexDirection: 'row', marginTop: 13}}>
+                                <Text style={{ fontSize: 18, fontWeight: '700'}}>{'Total distance'}</Text>
+                                <Text style={{ marginLeft: 20, fontSize: 18, fontWeight: '700', color: COLORS.primary}}>{'50'}</Text>
+                                <Text style={{ marginLeft: 5, fontSize: 18, fontWeight: '700', color: COLORS.primary}}>km</Text>
+                            </View>
+                            <View style={{flex: 1, flexDirection: 'row', marginTop: 13}}>
+                                {/* <Text style={{ fontSize: 18, fontWeight: '700'}}>{'Total distance'}</Text> */}
+                                <ProgressBarAndroid
+                                    styleAttr="Horizontal"
+                                    indeterminate={false}
+                                    progress={0.5}
+                                    height={10}
+                                    style={{width: '100%'}}
+                                    />
+                                {/* <Text style={{ marginLeft: 5, fontSize: 18, fontWeight: '700', color: COLORS.primary}}>km</Text> */}
+                            </View>
+                        </View>
+                    </View>
+                </View>
+                <Text style={styles.sectionTitle}>Friend</Text>
+            </ScrollView>
+        </SafeAreaView>
+        // </SafeAreaProvider>
     );
 };
-
-export default DashboardScreen;
 
 const styles = StyleSheet.create({
     container: {
@@ -21,4 +75,26 @@ const styles = StyleSheet.create({
         alignItems: 'center',
         justifyContent: 'center'
     },
+    header: {
+        paddingHorizontal: 15,
+        flexDirection: 'row',
+        justifyContent: 'space-between',
+        backgroundColor: COLORS.primary,
+    },
+    profileContainer: {
+        height: 140, width: '100%',
+        backgroundColor: 'white',
+        borderRadius: 18,
+        position: 'absolute',
+        top: 30,
+        paddingHorizontal: 25
+    },
+    sectionTitle: {
+        marginHorizontal: 30,
+        fontWeight: 'bold',
+        fontSize: 20,
+        marginVertical: 40,
+    }
 });
+
+export default DashboardScreen;
